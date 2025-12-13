e32157d7b89f3aae633a595787bd353e68449a9d
avaloscornel-oss <avaloscornel@gmail.com>
2025-12-13 09:21:53 +0200

    Update infinity-challenge.js

--- fe000000/js/infinity/infinity-challenge.js
+++ fe000000/js/infinity/infinity-challenge.js
@@ -4,7 +4,7 @@ let InfinityChallenge = {
     Decimal.pow(2, 24576), Decimal.pow(2, 20480), Decimal.pow(2, 22528), Decimal.pow(2, 57344),
   ],
   requirements: [Infinity,
-    Decimal.pow(2, 8192), Decimal.pow(2, 13500), Decimal.pow(2, 32768), Decimal.pow(2, 36864),
+    Decimal.pow(2, 8192), Decimal.pow(2, 13500), Decimal.pow(2, 21768), Decimal.pow(2, 36864),
     Decimal.pow(2, 49152), Decimal.pow(2, 53248), Decimal.pow(2, 57344), Decimal.pow(2, 61440),
   ],
   infinityChallengeButtonText(x) {
