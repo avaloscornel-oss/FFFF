908c8b28eb4f1e78f3e4cda6714d07eaa3c42f38
avaloscornel-oss <avaloscornel@gmail.com>
2025-12-13 08:36:12 +0200

    Update infinity-upgrades.js

--- fe000000/js/infinity/infinity-upgrades.js
+++ fe000000/js/infinity/infinity-upgrades.js
@@ -10,10 +10,10 @@ let InfinityUpgrade = function (i) {
       player.infinityUpgrades[i - 1] += n;
     },
     boughtLimit() {
-      return [Infinity, 8][i - 1];
+      return [Infinity, 24][i - 1];
     },
     costIncreasePer() {
-      return [8, 16][i - 1];
+      return [6, 16][i - 1];
     },
     effectIncreasePer() {
       return [0.25, 0.125][i - 1];
